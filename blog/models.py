1767f9c5884e698e197843838c55daf750a50752
NohaAboutaleb <nohamesllhy133@gmail.com>
2023-11-27 16:13:56 +0200

    Blog and category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Post(models.Model):
+
+    category = models.ForeignKey(
+        Category, on_delete=models.PROTECT, default=1)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+    attachment = models.FileField(upload_to='attachments/', null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
