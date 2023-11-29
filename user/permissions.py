354c35a4ce04c4635dec7b800922d7d69e9ec2da
NohaAboutaleb <nohamesllhy133@gmail.com>
2023-11-29 02:19:02 +0200

    permission groups

--- user/permissions.py
+++ user/permissions.py
@@ -1,17 +1,21 @@
 # permissions.py
-from rest_framework import permissions
+from rest_framework.permissions import BasePermission
 
-class AdminPermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return request.user.groups.filter(name='Admin').exists()
 
-class AuthorPermission(permissions.BasePermission):
+class AdminPermission(BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='Author').exists()
+        return request.user.groups.filter(name="AdminGroup").exists()
+
+
+class AuthorPermission(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="AuthorGroup").exists()
+
     def has_object_permission(self, request, view, obj):
         # Allow the author to update or delete their own posts
         return obj.author == request.user
 
-class ReaderPermission(permissions.BasePermission):
+
+class ReaderPermission(BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='Reader').exists()
+        return request.user.groups.filter(name="ReaderGroup").exists()
